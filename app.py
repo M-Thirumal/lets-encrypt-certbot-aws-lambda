fbf002eb409f3a324d95bd699bef0d58b93cdbab
Thirumal <m.thirumal@hotmail.com>
2022-09-11 20:24:43 +0530

    A

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import logging
 from chalice import Chalice
 
 app = Chalice(app_name='lets-encrypt-certbot-aws-lambda')
 
+app = Chalice(app_name='lets-encrypt-certbot-aws-lambda', debug=True, configure_logs=True)
+app.debug = True
+logging.getLogger().setLevel(logging.DEBUG)
+
 
 @app.lambda_function()
-def renew_tls(event, context):
+def renew_tls(event):
+    logging.debug("Received an event {}".format(event))
     return {'hello': 'world'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,5 +4,5 @@ from app import app
 
 def test_index():
     with Client(app) as client:
-        response = client.lambda_.invoke('first_function', {})
-        assert response.payload == {'hello': 'world'}
+        response = client.lambda_.invoke('renew_tls', {})
+        assert response.payload is not None
